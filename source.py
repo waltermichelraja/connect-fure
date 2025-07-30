2652011d4edc392f6a3f80ed8368740a1743f4d6
Walter Michel Raja <85942041+waltermichelraja@users.noreply.github.com>
2025-07-30 09:01:25 +0530

    initial commit 0.0.2

--- source.py
+++ source.py
@@ -1,16 +1,16 @@
 #bai i think we could go with matrix first then we alter it
 #stack class
-class stack:
+class Stack:
     def __init__(self):
         self.stack=[]
-    def push(self,c):
+    def push(self, c):
         self.stack.append(c)
     def pop(self):
         self.stack.pop()
     def peek(self):
         return self.stack[-1]
         
-s=stack()#dont worry used to test the snipet can delete it later
+s=Stack() #dont worry used to test the snipet can delete it later
 s.push(5)
 print(s.stack)
 
@@ -19,31 +19,29 @@ class board:
     def __init__(self):
         self.board=[]
         for i in range(8):
-            ss=stack()
+            ss=Stack()
             self.board.append(ss)
     
 #i dont know how to format stuffs you do it
-def flowcheck(bo,pl):
+def flowcheck(bo, pl):
     if i==1:  
         bo.board[pl-1].push(2)
     else:
         bo.board[pl-1].push(3)
     while bo.board[pl-1]:
-        c=1;
-        bo.board[pl-1].peak()#bro istop here i am goin to sleep
-        
-    
-    
+        c=1
+        bo.board[pl-1].peak() #bro i stop here i am goin to sleep
+
 i=1
 b=board()
 while True:
     if i==1:
-        p=int(input("Enter"))
-        flowcheck(b,p)
+        p=int(input("enter"))
+        flowcheck(b, p)
         i+=1
     else:
-        p1=int(input("Enter"))
-        flowchart(b,p1)
+        p1=int(input("enter"))
+        flowcheck(b, p1)
         i-=1
      
 
