f91007844ec811e1a0238ba7068a1d3f552bbb83
Walter Michel Raja <85942041+waltermichelraja@users.noreply.github.com>
2025-08-07 23:29:00 +0530

    logic commit 0.2.1

--- backend/logic.py
+++ backend/logic.py
@@ -5,13 +5,20 @@ COLUMNS=8
 EMPTY="."
 PLAYER_SYMBOLS=["X", "O"]
 
+class User:
+    def __init__(self, username):
+        self.id=str(uuid.uuid4())
+        self.username=username
+        self.games=[]
+
 class Connect4:
-    def __init__(self):
+    def __init__(self, player_id1, player_id2):
         self.id=str(uuid.uuid4())
         self.board=[[EMPTY for _ in range(COLUMNS)] for _ in range(ROWS)]
         self.turn=0
         self.winner=None
-        self.status=1
+        self.status=1 # game-over: 0 | draw: -1 | in-progress: 1
+        self.players=[player_id1, player_id2]
 
     def is_valid_column(self, col):
         return 0<=col<COLUMNS and self.board[0][col]==EMPTY
